07aa8fd642436defe32ff8d1b5c4b1e39cc19269
krishnareddy219 <121341000+krishnareddy219@users.noreply.github.com>
2023-06-25 15:33:04 +0530

    Update main.tf

--- Terraformfiles/main.tf
+++ Terraformfiles/main.tf
@@ -1,12 +1,13 @@
 resource "aws_amplify_app" "example" {
   name       = "nextjs"
-  repository = "https://github.com/Kiranteja623/nextjs-s3-upload.git"
-  auto_branch_creation_patterns = [true] 
+  repository = "https://github.com/krishnareddy219/nextjs-s3-upload.git"
+  access_token = "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"
+  auto_branch_creation_patterns = [true]
   enable_auto_branch_creation   = true
   enable_basic_auth = false
   enable_branch_auto_deletion = false
   environment_variables         = {}
-  iam_service_role_arn          = "arn:aws:iam::923869565259:role/service-role/AmplifySSRLoggingRole-ddksw3xo98puf"
+  iam_service_role_arn          = "arn:aws:iam::127347583409:role/amplifyconsole-backend-role"
   tags = {
      name= "nextjs"
    }
@@ -36,4 +37,3 @@ resource "aws_amplify_webhook" "master" {
   branch_name = aws_amplify_branch.master.branch_name
   description = "triggermaster"
 }
-
